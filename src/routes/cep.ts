df3bf68180ff571573c25ebb67126eb462dc0364
Larner Diogo <larnerdoj@hotmail.com>
2024-03-12 22:02:46 -0300

    Adicao V1 api

--- src/routes/cep.ts
+++ src/routes/cep.ts
@@ -6,7 +6,9 @@ const endpointV1 = "/cep/v1/";
 const endpointV2 = "/cep/v2/";
 
 export const getBy = (cep: string, version: string = "v2") => {
-  return version === "v2"
-    ? get<CepV2>(endpointV2 + onlyNumbers(cep))
-    : get<CepV1>(endpointV1 + onlyNumbers(cep));
+  if (version === "v2") {
+    return get<CepV2>(endpointV2 + onlyNumbers(cep));
+  } else {
+    return get<CepV1>(endpointV1 + onlyNumbers(cep));
+  }
 };
